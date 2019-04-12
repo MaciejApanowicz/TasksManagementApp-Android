c2fd3890041bbbab056330b9f5e92218a2a379b2
MaciejApanowicz <maciej.apanowicz@gmail.com>
2019-04-12 13:50:01 +0200

    Toolbar and a fragment added to TaskListActivity

--- app/src/main/java/pl/maciejapanowicz/tasksmanager/activity/TaskListActivity.java
+++ app/src/main/java/pl/maciejapanowicz/tasksmanager/activity/TaskListActivity.java
@@ -2,6 +2,7 @@ package pl.maciejapanowicz.tasksmanager.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 public class TaskListActivity extends Activity {
 
@@ -9,5 +10,6 @@ public class TaskListActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_list);
+        setActionBar((Toolbar) findViewById(R.id.toolbar));
     }
 }
