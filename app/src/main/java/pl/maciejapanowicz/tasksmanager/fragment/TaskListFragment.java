5830f17f46d4f79d6e61b6862569d3cbfb6f5fa3
MaciejApanowicz <maciej.apanowicz@gmail.com>
2019-04-19 22:09:01 +0200

    RecyclerView implemented

--- app/src/main/java/pl/maciejapanowicz/tasksmanager/fragment/TaskListFragment.java
+++ app/src/main/java/pl/maciejapanowicz/tasksmanager/fragment/TaskListFragment.java
@@ -2,29 +2,31 @@ package pl.maciejapanowicz.tasksmanager.fragment;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import pl.maciejapanowicz.tasksmanager.activity.R;
 
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class TaskListFragment extends Fragment {
 
+    RecyclerView recyclerView;
 
     public TaskListFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(
-                R.layout.fragment_task_list, container, false);
-    }
 
+        final View view = inflater.inflate(R.layout.fragment_task_list, container, false);
+        recyclerView = (RecyclerView) view.findViewById(R.id.recycler);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity())
+        );
+        return view;
+    }
 }
