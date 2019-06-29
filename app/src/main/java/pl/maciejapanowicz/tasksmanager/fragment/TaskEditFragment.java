8cb1a2dee78f3996ea50631ae77a60321b8f9c12
MaciejApanowicz <maciej.apanowicz@gmail.com>
2019-06-29 22:05:30 +0200

    menu overflow button appearance amendment

--- app/src/main/java/pl/maciejapanowicz/tasksmanager/fragment/TaskEditFragment.java
+++ app/src/main/java/pl/maciejapanowicz/tasksmanager/fragment/TaskEditFragment.java
@@ -77,7 +77,7 @@ public class TaskEditFragment extends Fragment {
     }
 
     @Override
-    public void onCreateOptionsMenu (Menu menu, MenuInflater inflater){
+        public void onCreateOptionsMenu (Menu menu, MenuInflater inflater){
         super.onCreateOptionsMenu(menu, inflater);
 
         menu.add(0, MENU_SAVE, 0, R.string.save)
--- app/src/main/java/pl/maciejapanowicz/tasksmanager/fragment/TaskListFragment.java
+++ app/src/main/java/pl/maciejapanowicz/tasksmanager/fragment/TaskListFragment.java
@@ -49,15 +49,23 @@ public class TaskListFragment extends Fragment {
     }
 
     @Override
+    public void onViewCreated (View view, Bundle savedInstanceState){
+        super.onViewCreated(view, savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
     public void onCreateOptionsMenu (Menu menu, MenuInflater inflater){
-        super.onCreateOptionsMenu(menu, inflater);
         inflater.inflate(R.menu.menu_list, menu);
+        //super.onCreateOptionsMenu(menu, inflater)   ;
+
     }
 
     @Override
     public boolean onOptionsItemSelected (MenuItem item){
-        if (item.getItemId() == R.id.menu_insert ){
-            ((OnTaskEdit) getActivity()).editTask(0);
+        switch (item.getItemId()){
+            case R.id.action_add:
+                    ((OnTaskEdit) getActivity()).editTask(0);
             return true;
         }
         return super.onOptionsItemSelected(item);
