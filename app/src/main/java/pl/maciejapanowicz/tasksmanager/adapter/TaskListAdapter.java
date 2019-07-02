fb76c91521990c7aa5792bfa88538d372d19806d
MaciejApanowicz <maciej.apanowicz@gmail.com>
2019-07-02 23:27:08 +0200

    add strings for deleting

--- app/src/main/java/pl/maciejapanowicz/tasksmanager/adapter/TaskListAdapter.java
+++ app/src/main/java/pl/maciejapanowicz/tasksmanager/adapter/TaskListAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -62,6 +63,10 @@ public class TaskListAdapter extends RecyclerView.Adapter<TaskListAdapter.ViewHo
         return downloadPicturesFromThisUrl+taskIdNumber+imagesSize;
     }
 
+    void deleteTask(Context context, long id ) {
+        Log.d("TaskListAdapter", "Called deleteTask" );
+    }
+
     static class ViewHolder extends RecyclerView.ViewHolder {
         CardView cardView;
         TextView titleView;
@@ -74,4 +79,6 @@ public class TaskListAdapter extends RecyclerView.Adapter<TaskListAdapter.ViewHo
             imageView = (ImageView) singleCard.findViewById(R.id.image);
         }
     }
+
+
 }
